ba6d60e3b98e81c0b943a0c5caaade4d850349d6
Terlick <meozkara@gmail.com>
2022-02-18 14:08:26 +0000

    Delete unnecessary files

--- twspace_dl/__init__.py
+++
@@ -1,5 +0,0 @@
-from .format_info import FormatInfo
-from .twspace_dl import TwspaceDL
-from .login import Login, load_from_file, write_to_file
-
-__all__ = ["FormatInfo", "TwspaceDL", "Login", "load_from_file", "write_to_file"]
